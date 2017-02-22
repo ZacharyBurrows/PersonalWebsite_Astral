10161e680a1994e6bb711d32b1dc0f04a5c25c11
ZacharyBurrows <zburrows7@gmail.com>
2017-02-22 15:59:57 -0800

    testing ark updater...

--- ark/arkupdater.php
+++ ark/arkupdater.php
@@ -11,13 +11,14 @@ if (isset($_POST['number1']) && isset($_POST['number2'])) {
 <html>
 <body>
 
-    <?php if (isset($result)) { ?>
-        <h1> Result: <?php echo $result ?></h1>
-    <?php } ?>
     <form action="" method="post">
     <p>1-st number: <input type="text" name="number1" /></p>
     <p>2-nd number: <input type="text" name="number2" /></p>
     <p><input type="submit"/></p>
 
+
+    <?php if (isset($result)) { ?>
+        <textarea rows="20" cols="20"> <?php echo $result ?></textarea>
+    <?php } ?>
 </body>
 </html>
