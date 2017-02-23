f70bfb175a70225070294bdf4828508c2f638cd3
root <root@raspberrypi.(none)>
2017-02-23 05:29:27 +0000

    I think its working

--- ark/ssh_command.sh
+++ ark/ssh_command.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sshpass -p "dickbutt" ssh -t -t ark:dickbutt@73.181.247.193 /home/ark/epicsrvrmgr/ark/stopUpdateStart.sh
+sshpass -p "dickbutt" ssh -tt ark:dickbutt@73.181.247.193 << ENDHERE
+cd /home/ark/epicsrvrmgr/ark
+./stopUpdateStart.sh
+ENDHERE
