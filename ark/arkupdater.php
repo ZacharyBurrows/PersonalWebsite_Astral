a771820868c52899d7addf5ec15aaf6aea60ae51
root <root@raspberrypi.(none)>
2017-02-27 22:21:08 +0000

    added some stuff to the server update website

--- ark/arkupdater.php
+++ ark/arkupdater.php
@@ -16,11 +16,22 @@
 ?>
 
 <html>
-<style>
-	background-image: url("http://i.imgur.com/nKtB4E2.png");
-</style>
+	<head>
+	<style>
+		body {
+			background-image: url("http://i.imgur.com/nKtB4E2.png");
+		}
+	</style>
+	</head>
 
-<body style="background-image: url("http://i.imgur.com/nKtB4E2.png");">
+<body >
+
+	<h1>ARK UPDATE SITE</h1>
+	<p>Stops, updates, and starts server</br>
+	Please be patient. Update can take a few minutes.</br>
+	--- </br>
+	Good luck out there...</br>
+	---</p>
 
     <form action="" method="post">
     <p>password: <input type="text" name="password" /></p>
